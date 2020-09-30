ba0af880b90d8e21a30b5a8de82f898dfdfbe6f6
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-30 16:00:42 +0545

    Add and list items

--- stockmanagement/views.py
+++ stockmanagement/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from.forms import StockCreateForm
 
-# Create your views here.
+
+def home(request):
+    title = "Welcome to home page"
+    context = {
+        'title': title
+    }
+    return render(request, 'home.html', context)
+
+
+def list_item(request):
+    title = "List"
+    queryset = Stock.objects.all()
+    context = {
+        'title': title,
+        'queryset': queryset
+    }
+    return render(request, 'list_items.html', context)
+
+
+def add_items(request):
+    form = StockCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/list_items')
+    context = {
+        "form": form,
+        "title": "Add item",
+    }
+    return render(request, "add_items.html", context)
